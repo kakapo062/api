92dcb6c152306b8107699f587d07cebae02ce550
baumkuchen <koki@mba.local>
2023-04-22 23:55:03 +0900

    fix cors problem

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative "boot"
 require "rails/all"
 require "graphiql/rails" # 追加する
 require "sprockets/railtie"
+require "rack/cors" # 追加する
 
 Bundler.require(*Rails.groups)
 
@@ -18,5 +19,18 @@ module Myapp
     config.middleware.use(Rack::Deflater)
     config.assets.enabled = true
     config.assets.paths << Rails.root.join("node_modules")
+
+    # CORS設定を追加する
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000' # フロントエンドのオリジンを指定 (Next.jsのデフォルトポートは3000)
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head], # 許可するHTTPメソッドを指定
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], # 任意で追加するヘッダー情報
+          credentials: true
+      end
+    end
   end
 end
